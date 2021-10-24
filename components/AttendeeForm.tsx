81477f74e6bd6d8c7135476f047265304106acc6
William Shepherd <will@codeincolor.io>
2021-10-23 21:40:54 -0500

    Extract types

--- components/AttendeeForm.tsx
+++ components/AttendeeForm.tsx
@@ -2,14 +2,7 @@ import React, { useState } from "react";
 import * as Yup from "yup";
 import { withFormik, FormikProps, FormikErrors, Form, Field } from "formik";
 import ToggleSwitch from "./ToggleSwitch";
-
-interface FormValues {
-  fullname: string;
-  email: string;
-  notificationWhenVideoPublished: boolean;
-  rateMyPresentation: boolean;
-  notificationOfOtherTalks: boolean;
-}
+import { PresentationDownloadForm as FormValues } from "./types/survey";
 
 interface AttendeeFormProps {
   initialName?: string;
---
+++ components/types/survey.ts
@@ -0,0 +1,7 @@
+export interface PresentationDownloadForm {
+  fullname: string;
+  email: string;
+  notificationWhenVideoPublished: boolean;
+  rateMyPresentation: boolean;
+  notificationOfOtherTalks: boolean;
+}
