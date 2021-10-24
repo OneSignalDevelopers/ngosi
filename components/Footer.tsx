6b4aa4e643049380eafec0d4445910be56981e05
William Shepherd <will@codeincolor.io>
2021-10-23 19:33:01 -0500

    Organize repetitive code into components

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => (
+  <footer className="flex flex-none justify-center p-2 h-12 bg-black text-sm">
+    <a
+      className="text-indigo-500 font-semibold tracking-wide"
+      href="#ngosi.io"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      ngosi
+    </a>
+  </footer>
+);
+
+export default Footer;
---
+++ components/PresentationInfo.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import presenter from "../public/images/presenter.jpg";
+
+interface Props {
+  /** The presenter's first name */
+  firstName: string;
+
+  /** The presenter's last name */
+  lastName: string;
+
+  /** The title of the presentation */
+  title: string;
+}
+
+const PresentationInfo: React.FC<Props> = ({ firstName, lastName, title }) => (
+  <div className="flex flex-col space-y-2">
+    <h2 className="capitalize font-semibold text-lg">
+      {firstName} {lastName}
+    </h2>
+    <div className="flex">
+      <div className="h-28 w-28 relative flex-none">
+        {" "}
+        <Image
+          src={presenter}
+          alt="Photo of presenter"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <p className="ml-2 px-2 overflow-ellipsis max-h-28">{title}</p>
+    </div>
+  </div>
+);
+
+export default PresentationInfo;
