9c9230d3486e941af58b80646eff9319b4b3768b
William Shepherd <will@codeincolor.io>
2021-10-23 21:41:57 -0500

    Scaffold out api
    This will accept survey responses

---
+++ pages/api/survey-response.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data =
+  | {
+      message: string;
+    }
+  | {
+      error: string;
+    };
+
+export default async function asynchandler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    console.log(req.body);
+
+    res.status(200).json({ message: "completed" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: (error as Error).message });
+  }
+}
