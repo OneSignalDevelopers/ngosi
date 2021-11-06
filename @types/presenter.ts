f2261af567dbaff86a1c0b0d1db6a241b086b672
William Shepherd <will@codeincolor.io>
2021-11-06 15:54:35 -0500

    Build data model for presentations

---
+++ @types/presenter.ts
@@ -0,0 +1,33 @@
+export interface Presenter {
+  /** A unique identifier for the presenter */
+  readonly id: string;
+
+  /** The presenter's first name */
+  readonly firstName: string;
+
+  /** The presenter's last name */
+  readonly lastName: string;
+
+  /** The presenter's email address */
+  readonly email: string;
+
+  /** A list of presentations the presenter has created in Ngosi */
+  readonly presentations: Presentation[];
+}
+
+export interface Presentation {
+  /** A unique identifier for a presentation */
+  readonly id: string;
+
+  /** The ID of the presenter who created this presentation */
+  readonly presenter: string;
+
+  /** The name of the presentation */
+  readonly title: String;
+
+  /** The location where the live talk took place */
+  readonly location?: String;
+
+  /** The name of the conf or event where the presentation was given */
+  readonly eventName: string;
+}
