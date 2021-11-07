a63a068643f11b7fe8b8d512d0b56eca6799f70f
William Shepherd <will@codeincolor.io>
2021-11-07 14:24:15 -0600

    Add a home page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen bg-primary pt-4 pl-6">
+      <h1 className="text-white text-6xl">ngosi</h1>
+    </div>
+  );
+};
+
+export default Home;
