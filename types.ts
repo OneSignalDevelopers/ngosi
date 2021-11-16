7e0220d4450bb66a27fa3ee66bcd80a966acbbe2
William Shepherd <will@codeincolor.io>
2021-11-16 00:40:05 -0600

    Refine types

--- types.ts
+++ types.ts
@@ -1,49 +1,70 @@
 export interface Presenter {
   /** A unique identifier for the presenter */
-  readonly id: string;
+  readonly id: string
 
   /** The presenter's first name */
-  readonly firstName: string;
+  readonly firstName: string
 
   /** The presenter's last name */
-  readonly lastName: string;
+  readonly lastName: string
 
   /** The presenter's email address */
-  readonly email: string;
+  readonly email: string
 
   /** The URL to the presenters profile photo */
-  readonly profileImage: string;
+  readonly profileImage: string
 
   /** A list of presentations the presenter has created in Ngosi */
-  readonly presentations: Preso[];
+  readonly presentations: Preso[]
 }
 
 export interface Preso {
-  /** A unique identifier for a presentation */
-  readonly id: string;
+  /** The publically visible unique identifier for the presentation.  */
+  readonly shortCode: string
 
-  /** The ID of the presenter who created this presentation */
-  readonly presenter: string;
+  /** The name of the presentation. */
+  readonly title: string
 
-  /** The name of the presentation */
-  readonly title: string;
+  /** The location where the event took place. */
+  readonly eventLocation?: string
 
-  /** The location where the live talk took place */
-  readonly location?: string;
+  /** The name of the event where the presentation was given. */
+  readonly eventName: string
 
-  /** The name of the conf or event where the presentation was given */
-  readonly eventName: string;
+  /** The url where the presentation can be found. */
+  readonly url: string
 }
 
 export interface SurveyForm {
-  fullname: string;
-  email: string;
-  notificationWhenVideoPublished: boolean;
-  rateMyPresentation: boolean;
-  notificationOfOtherTalks: boolean;
+  /** The survey respondent's first and last name. */
+  fullname: string
+
+  /** The survey respondent's email address. */
+  email: string
+
+  /** Indicates if the survey respondent
+   * should be sent an email when
+   * the recording of the presentation
+   * is published by the event host.
+   * */
+  notificationWhenVideoPublished: boolean
+
+  /** Indicates if the survey respondent
+   * should be sent an SMS with a
+   * link to a feedback form a few
+   * minutes after the live talk
+   * is scheduled to conclude.
+   */
+  rateMyPresentation: boolean
+
+  /** Indicates if the survey respondent
+   * should be notified whenever
+   * the presenter presents again.
+   */
+  notificationOfOtherTalks: boolean
 }
 
 export interface PresoForm {
-  /** The URL to access the presener's slides */
-  url: string;
+  /** The Url where the presener's slides are located */
+  url: string
 }
