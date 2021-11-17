c34720545bce55b979cf070f3e2549f715753720
Jordan Adler <jordan.m.adler@gmail.com>
2021-11-17 14:14:48 -0800

    Add OneSignal React SDK to ngosi frontend app

--- common/utils.ts
+++ common/utils.ts
@@ -1,1 +1,23 @@
-export type Writeable<T> = { -readonly [P in keyof T]: T[P] }
+import { useEffect } from "react";
+import { OneSignalAppId } from "@common/constants"
+
+export const useOneSignal = () =>
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: OneSignalAppId,
+        notifyButton: {
+          enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []); // <-- run this effect once on mount
+
+
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen bg-primary pt-4 pl-6">
+      <Head>
+        <script
+          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+          async=""
+        ></script>
+      </Head>
       <h1 className="text-white text-6xl">ngosi</h1>
     </div>
   );
