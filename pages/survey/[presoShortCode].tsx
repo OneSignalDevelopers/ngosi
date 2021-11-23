1018ead56383f3a670cbeded2ce7dbefd1bb8db4
William Shepherd <will@codeincolor.io>
2021-11-22 20:27:33 -0600

    Fix form to work with survey-response endpoint

--- pages/survey/[presoShortCode].tsx
+++ pages/survey/[presoShortCode].tsx
@@ -1,8 +1,6 @@
-import { Presenter } from '.prisma/client'
 import EventHeader from '@components/EventHeader'
 import FatalError from '@components/FatalError'
 import Footer from '@components/Footer'
-import PresentationInfo from '@components/PresentationInfo'
 import SurveyForm from '@components/SurveyForm'
 import { Preso } from '@types'
 import { NextPage } from 'next'
@@ -14,19 +12,9 @@ import type { SurveyForm as ISurveyForm } from 'types'
 const Survey: NextPage = () => {
   const router = useRouter()
   const [preso, setPreso] = useState<Preso | null>(null)
-  const [presenter, setPresenter] = useState<Presenter | null>(null)
-  const { presoShortCode } = router.query
-
-  const onSurveySubmit = async (values: ISurveyForm) => {
-    const response = await fetch('/api/survey-response', {
-      method: 'POST',
-      body: JSON.stringify({ ...values, presoShortCode })
-    })
-
-    console.log('SurveyFormSubmit', response)
-  }
 
   useEffect(() => {
+    const { presoShortCode } = router.query
     if (!presoShortCode || typeof presoShortCode !== 'string') {
       return
     }
@@ -38,27 +26,28 @@ const Survey: NextPage = () => {
       })
 
       const json = await response.json()
-      const { preso, presenter } = json
-      if (preso && presenter) {
+      const { preso } = json
+      if (preso) {
         setPreso(preso)
-        setPresenter(presenter)
       }
     }
 
     fetchPresentation()
-  }, [presoShortCode])
+  }, [router.query])
+
+  const onSurveySubmit = async (values: ISurveyForm) => {
+    const response = await fetch('/api/survey-response', {
+      method: 'POST',
+      body: JSON.stringify({
+        ...values,
+        presoShortCode: router.query.presoShortCode
+      })
+    })
+  }
 
   if (!preso) {
     return <FatalError message="Presentation doesn't exist" />
   }
-
-  if (!presenter) {
-    return (
-      <FatalError message="Presenter could not be found. This is a big problem." />
-    )
-  }
-
-  const { firstName, lastName } = presenter
 
   return (
     <div className="flex flex-col min-h-screen min-w-full">
@@ -71,15 +60,15 @@ const Survey: NextPage = () => {
 
       <main className="flex flex-col flex-1 mt-2 space-y-3">
         <div className="px-8 py-2">
-          <PresentationInfo
-            firstName={firstName}
+          {/* <PresentationInfo
+            firstName={preso}
             lastName={lastName}
             title={preso.title}
-          />
+          /> */}
         </div>
         <div className="w-full bg-gray-50">
           <div className="px-8 py-3">
-            <SurveyForm onSubmit={onSurveySubmit} />
+            <SurveyForm onSubmit={(values) => onSurveySubmit(values)} />
           </div>
         </div>
       </main>
