7a2af27d1ed0c8f04980774b966ff90d8e9a2a08
William Shepherd <will@codeincolor.io>
2022-01-02 19:17:44 -0600

    Remove deprecated option
    This fixes a warning emitted by Tailwind 3.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
