eac3530933364d80e1ba010c2cb7fa62222e2891
William Shepherd <will@codeincolor.io>
2022-01-05 15:48:14 -0600

    Add scaffolding for preso detail page

---
+++ pages/preso/[id].tsx
@@ -0,0 +1,26 @@
+import { useSupabase } from '@common/supabaseProvider'
+import { GetServerSideProps, NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const PresoDetails: NextPage = () => {
+  const router = useRouter()
+  const { client: supabaseClient } = useSupabase()
+  const { id: presoId } = router.query
+
+  useEffect(() => {
+    // fetch preso details
+  }, [])
+
+  return <pre>{JSON.stringify(router.query, null, 2)}</pre>
+}
+
+// export const getServerSideProps: GetServerSideProps = (context) => {
+//   const { id: presoId } = context.query
+
+//   return {
+//     props: {}
+//   }
+// }
+
+export default PresoDetails
