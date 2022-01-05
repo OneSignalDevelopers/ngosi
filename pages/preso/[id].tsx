3bc99eb0ee174d5bd063a2624d81003c3d58bd32
William Shepherd <will@codeincolor.io>
2022-01-05 15:59:53 -0600

    Fetch preso on load

--- pages/preso/[id].tsx
+++ pages/preso/[id].tsx
@@ -1,18 +1,42 @@
 import { useSupabase } from '@common/supabaseProvider'
-import { GetServerSideProps, NextPage } from 'next'
+import { Preso } from '@types'
+import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const PresoDetails: NextPage = () => {
   const router = useRouter()
-  const { client: supabaseClient } = useSupabase()
-  const { id: presoId } = router.query
+  const { client } = useSupabase()
+  const { id: shortCode } = router.query
+  const [preso, setPreso] = useState<Preso | null>(null)
 
   useEffect(() => {
-    // fetch preso details
-  }, [])
+    async function getPresoDetails() {
+      if (typeof shortCode !== 'string') {
+        return
+      }
 
-  return <pre>{JSON.stringify(router.query, null, 2)}</pre>
+      const { data, error } = await client
+        .from<Preso>('Preso')
+        .select()
+        .eq('shortCode', shortCode)
+        .single()
+
+      if (error) {
+        console.error(error)
+        return
+      }
+
+      if (data) {
+        console.log(data)
+        setPreso(data)
+      }
+    }
+
+    getPresoDetails()
+  }, [client, shortCode])
+
+  return <pre>{JSON.stringify(preso, null, 2)}</pre>
 }
 
 export default PresoDetails
