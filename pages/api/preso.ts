07acbe44c7d021bcf588624d9253ac368abb4a26
William Shepherd <will@codeincolor.io>
2022-01-12 16:48:45 -0600

    Add API method for preso updates

--- pages/api/preso.ts
+++ pages/api/preso.ts
@@ -1,33 +1,55 @@
-import { Preso, PresoForm } from '@types'
+import { Preso, PresoDetails, PresoForm } from '@types'
 import cuid from 'cuid'
 import { nanoid } from 'nanoid'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { StatusCodes } from 'http-status-codes'
 import { supabaseClient } from './common/supabase'
 
-type Data =
+type PresoPostResp =
   | {
+      success: true
       presoShortCode: string
     }
   | {
+      success: false
       error: string
     }
+
+type PresoPutResp =
+  | {
+      success: true
+    }
+  | { success: false; error: string }
 
 /**
  * Create a preso.
  */
 export default async function asynchandler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<PresoPostResp | PresoPutResp>
+) {
+  switch (req.method) {
+    case 'PUT':
+      return await updatePreso(req, res)
+    case 'POST':
+      return await addPreso(req, res)
+  }
+}
+
+async function addPreso(
+  req: NextApiRequest,
+  res: NextApiResponse<PresoPostResp>
 ) {
   try {
     const { url, userId, eventName, title, eventLocation } = JSON.parse(
       req.body
     ) as PresoForm
+
     if (!url) {
-      res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: 'Presentation url is required.' })
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        error: 'Presentation url is required.'
+      })
     }
 
     const { data, error } = await supabaseClient
@@ -46,12 +68,39 @@ export default async function asynchandler(
     if (error) {
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: JSON.stringify(error) })
+        .json({ success: false, error: JSON.stringify(error) })
     } else {
-      res.status(StatusCodes.OK).json({ presoShortCode: data?.shortCode || '' })
+      res
+        .status(StatusCodes.OK)
+        .json({ success: true, presoShortCode: data?.shortCode || '' })
     }
   } catch (error) {
     const { message } = error as Error
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: message })
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error: message })
   }
+}
+
+async function updatePreso(
+  req: NextApiRequest,
+  res: NextApiResponse<PresoPutResp>
+) {
+  try {
+    const { url, eventName, title, eventLocation, publishedContentUrl, id } =
+      JSON.parse(req.body) as PresoDetails
+
+    const { data, error } = await supabaseClient
+      .from<Preso>('Preso')
+      .update({
+        eventName: eventName,
+        eventLocation: eventLocation,
+        title: title,
+        url: url
+      })
+      .match({ id: id })
+      .single()
+
+    res.status(StatusCodes.OK).json({ success: true })
+  } catch (error) {}
 }
