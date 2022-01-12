390ab6349cefe089e81b444c22badf0a769586ed
William Shepherd <will@codeincolor.io>
2022-01-12 15:55:37 -0600

    Add page to render preso details

--- pages/preso/[presoShortCode].tsx
+++ pages/preso/[presoShortCode].tsx
@@ -8,38 +8,44 @@ import { useEffect, useState } from 'react'
 const PresoDetails: NextPage = () => {
   const router = useRouter()
   const { client } = useSupabase()
-  const { id: shortCode } = router.query
+  const { presoShortCode } = router.query
   const [preso, setPreso] = useState<Preso | null>(null)
 
   useEffect(() => {
     async function getPresoDetails() {
-      if (typeof shortCode !== 'string') {
+      if (typeof presoShortCode !== 'string') {
         return
       }
 
       const { data, error } = await client
         .from<Preso>('Preso')
         .select()
-        .eq('shortCode', shortCode)
+        .eq('shortCode', presoShortCode)
         .single()
 
       if (error) {
         console.error(error)
-        return
       }
 
       if (data) {
-        console.log(data)
         setPreso(data)
       }
     }
 
     getPresoDetails()
-  }, [client, shortCode])
+  }, [client, presoShortCode])
 
   return preso ? (
     <div>
-      <Details preso={preso} onSubmit={async () => Promise.resolve(console.log("Save button clicked")) }/>
+      <Details
+        preso={preso}
+        onSubmit={async () =>
+          Promise.resolve(console.log('Save button clicked'))
+        }
+        onViewSurvey={(presoId) => {
+          router.push(`/response/${presoId}`)
+        }}
+      />
     </div>
   ) : (
     <div></div>
