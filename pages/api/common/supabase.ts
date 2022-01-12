98be7a27a5542df367c4434b32ed3c633e97bc4d
William Shepherd <will@codeincolor.io>
2022-01-12 17:04:27 -0600

    Subscribe to preso table events

--- pages/api/common/supabase.ts
+++ pages/api/common/supabase.ts
@@ -2,3 +2,10 @@ import { SupabaseUrl, SupabaseAnonKey } from '@common/constants'
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseClient = createClient(SupabaseUrl, SupabaseAnonKey)
+
+const subscriptionToPublishedVideos = supabaseClient
+  .from('Preso')
+  .on('*', (payload) => {
+    console.log('Change received!', payload)
+  })
+  .subscribe()
