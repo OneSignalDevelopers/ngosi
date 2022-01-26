5bc3aa0091f0bd65622987af1fea5afdd1eef302
William Shepherd <will@codeincolor.io>
2022-01-26 16:41:41 -0600

    Only listen for updates

--- pages/api/common/supabase.ts
+++ pages/api/common/supabase.ts
@@ -1,11 +1,12 @@
 import { SupabaseUrl, SupabaseAnonKey } from '@common/constants'
 import { createClient } from '@supabase/supabase-js'
+import * as OneSignal from 'onesignal-node'
 
 export const supabaseClient = createClient(SupabaseUrl, SupabaseAnonKey)
 
 const subscriptionToPublishedVideos = supabaseClient
   .from('Preso')
-  .on('*', async (payload) => {
+  .on('UPDATE', async (payload) => {
     // Ideally we can inspect the diff of the change and only
     // react if there's a change.
     const { id: presoId, publishedContentUrl } = payload.new
