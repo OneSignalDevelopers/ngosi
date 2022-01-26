b02413bb887652028009e161c3cbabd05b9b564f
William Shepherd <will@codeincolor.io>
2022-01-26 15:58:19 -0600

    Look up the attendees for the preso
    I'm filtering by whether they want to be notified of published videos.

--- pages/api/common/supabase.ts
+++ pages/api/common/supabase.ts
@@ -5,7 +5,17 @@ export const supabaseClient = createClient(SupabaseUrl, SupabaseAnonKey)
 
 const subscriptionToPublishedVideos = supabaseClient
   .from('Preso')
-  .on('*', (payload) => {
-    console.log('Change received!', payload)
+  .on('*', async (payload) => {
+    // Ideally we can inspect the diff of the change and only
+    // react if there's a change.
+    const { id: presoId, publishedContentUrl } = payload.new
+    const { error, data: presoAttendees } = await supabaseClient
+      .from('attendees_view')
+      .select('email, attendee')
+      .match({ preso: presoId, notifyWhenVideoPublished: true })
+
+    error
+      ? console.error(error)
+      : console.log('Attendee emails', presoAttendees)
   })
   .subscribe()
