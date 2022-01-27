5fdcbea5b0b27e9c29a109a9d1d97228b688eaec
Jordan Adler <jordan.m.adler@gmail.com>
2022-01-26 17:16:46 -0800

    Add basic linking to HeaderBar

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
+
+
 const HeaderBar = () => (
   <nav className="flex flex-none justify-between p-2 h-12 bg-accent-primary text-sm text-black font-bold min-w-full px-6 py-3">
-    <span>Home</span>
-    <span>Presos</span>
-    <span>Surveys</span>
-    <span>Audience</span>
-    <span>Profile</span>
+    <Link href="/"><a>Home</a></Link>
+    <Link href="/presos"><a>Presos</a></Link>
+    <Link href="/surveys"><a>Surveys</a></Link>
+    <Link href="/groupies"><a>Audience</a></Link>
+    <Link href="/account"><a>Account</a></Link>
   </nav>
 )
 
