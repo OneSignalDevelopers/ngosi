e36978c8e2a463c7f3ac1b0a3b6474bbd9fd9625
William Shepherd <will@codeincolor.io>
2022-02-02 16:10:32 -0600

    Add object to hold email template info

--- pages/api/common/onesignal.ts
+++ pages/api/common/onesignal.ts
@@ -5,3 +5,14 @@ export const onesignalClient = new OneSignal.Client(
   OneSignalAppId,
   OneSignalApiKey
 )
+interface EmailTemplate {
+  readonly id: string
+  readonly subject: string
+}
+
+export const OneSignalEmailTemplates: Record<string, EmailTemplate> = {
+  videoPublished: {
+    id: 'e204560e-e737-451e-afe1-496a098046ce',
+    subject: 'A video has published'
+  }
+}
