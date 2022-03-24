428f3d1b316ba487a605a4d96f0f607130003ae5
Jordan Adler <jordan.m.adler@gmail.com>
2022-03-23 17:05:41 -0700

    Styling updates:
    * Update Footer
    * Create Anonymous/LoggedIn variants of HeaderBar
    * Add typography tailwind plugin
    * Enforce logged-in state on account, groupies, presos pages
    * Create initial marketing page for anonymous users
    * Add ngosi logo

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -1,23 +1,47 @@
+import { useSupabase } from '@common/supabaseProvider'
 import Link from 'next/link'
 
-const HeaderBar = () => (
+const HeaderBarAnonymous = () => (
+    <Link href="/signin"><a className="underline">Login</a></Link>
+);
+
+const HeaderBarLoggedIn = () => {
+  const { session, client: supabaseClient } = useSupabase();
+  return (<>
+        <Link href="/presos">
+          <a>Presos</a>
+        </Link>
+        <Link href="/surveys">
+          <a>Surveys</a>
+        </Link>
+        <Link href="/groupies">
+          <a>Audience</a>
+        </Link>
+        <Link href="/account">
+          <a>Account</a>
+        </Link>
+        <button
+            className=""
+            onClick={async () => {
+              const { error } = await supabaseClient.auth.signOut()
+              if (error) console.log('Error logging out:', error.message)
+            }}
+          >
+            Logout
+        </button>
+    </>);
+};
+
+const HeaderBar = () => {
+  const { session, client: supabaseClient } = useSupabase();
+  return (
   <nav className="flex flex-none justify-between p-2 h-12 bg-accent-primary text-sm text-black font-bold min-w-full px-6 py-3">
     <Link href="/">
-      <a>Home</a>
+      <a>ngosi</a>
     </Link>
-    <Link href="/presos">
-      <a>Presos</a>
-    </Link>
-    <Link href="/surveys">
-      <a>Surveys</a>
-    </Link>
-    <Link href="/groupies">
-      <a>Audience</a>
-    </Link>
-    <Link href="/account">
-      <a>Account</a>
-    </Link>
+    { session ? <HeaderBarLoggedIn /> : <HeaderBarAnonymous /> }
   </nav>
-)
+  );
+}
 
 export default HeaderBar
