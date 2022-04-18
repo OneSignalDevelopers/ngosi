d4304ecdb6f3e9f85cf1c0cdfc2a296efa007e3f
William Shepherd <will@codeincolor.io>
2022-04-18 18:42:39 -0500

    Redirect only if there's no session
    We should no longer get the random logout behaviors

--- pages/account.tsx
+++ pages/account.tsx
@@ -2,27 +2,22 @@ import { useSupabase } from '@common/supabaseProvider'
 import Account from '@components/Account'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 const AccountPage: NextPage = () => {
-  const router = useRouter();
-  const { authState, session, client: supabaseClient } = useSupabase()
+  const router = useRouter()
+  const { session, client: supabaseClient } = useSupabase()
 
   useEffect(() => {
-    if (authState !== 'authenticated') {
+    if (!session) {
       router.replace('/signin')
     }
-  }, [authState, router])
-
-  if (!session) {
-    return <div>No session</div>
-  }
+  }, [session, router])
 
   return (
     <>
       <h1 className="text-white text-6xl">Ngosi</h1>
-      <Account key={session.user?.id} session={session!} />
+      <Account key={session!.user?.id} session={session!} />
       <button
         className="h-12 bg-black text-white font-bold text-xl mt-5 w-80"
         onClick={async () => {
