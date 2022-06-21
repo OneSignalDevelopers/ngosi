5c776cf910441341fd38855c7575ef3e51513795
Yohanan-Negash <yoniget066@gmail.com>
2022-06-21 10:38:42 -0400

    Add formik logic and yup validation schema

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,10 +1,17 @@
 import { useSupabase } from '@common/supabaseProvider'
 import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 
 export default function Auth() {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
+  const [yupError, setyupError] = useState('')
   const { client: supabaseClient } = useSupabase()
+
+  const sendMagicLinkSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is Required')
+  })
 
   const handleLogin = async (email: string) => {
     try {
@@ -14,35 +21,50 @@ export default function Auth() {
       alert('Check your email for the login link!')
     } catch (error) {
       const e = error as any
-      alert(e.error_description || e.message)
+      setyupError(e.error_description || e.message)
     } finally {
       setLoading(false)
     }
   }
 
   return (
-    <>
-      <h1 className="text-4xl text-center">Ngosi</h1>
-      <p className=" mt-5">Sign in via magic link with your email below</p>
+    <Formik
+      initialValues={{
+        email: ''
+      }}
+      validationSchema={sendMagicLinkSchema}
+    >
+      {({ errors, touched }) => (
+        <Form className="flex flex-col">
+          <h1 className="text-4xl text-center">Ngosi</h1>
+          <p className=" mt-5">Sign in via magic link with your email below</p>
 
-      <input
-        className="h-9 border border-black text-lg px-2 mt-1 w-80"
-        type="email"
-        placeholder="Your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+          <Field
+            className="h-9 border border-black text-lg px-2 mt-1 w-80"
+            name="email"
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {(errors.email && touched.email) || yupError ? (
+            <div className="text-red-500 text-sm w-80 mt-1">
+              {yupError || errors.email}
+            </div>
+          ) : null}
 
-      <button
-        onClick={(e) => {
-          e.preventDefault()
-          handleLogin(email)
-        }}
-        className="h-14 bg-accent-primary text-black font-bold text-xl mt-5 w-80 shadow-xl rounded-lg"
-        disabled={loading}
-      >
-        <span>{loading ? 'Loading' : 'Send magic link'}</span>
-      </button>
-    </>
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              handleLogin(email)
+            }}
+            className="h-14 bg-accent-primary text-black font-bold text-xl mt-5 w-80 shadow-xl rounded-lg"
+            disabled={loading}
+          >
+            <span>{loading ? 'Loading' : 'Send magic link'}</span>
+          </button>
+        </Form>
+      )}
+    </Formik>
   )
 }
